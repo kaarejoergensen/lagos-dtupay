ae6b1347b9593defc3bc629c462603297a3bfbd9
Asge Darr <asgedarr@Asges-MBP.zeroconf.dk>
2019-01-19 16:40:13 +0100

    multiple cucumber tests added

--- dtupay-bank/src/test/java/cucumber/Stepdefs.java
+++ dtupay-bank/src/test/java/cucumber/Stepdefs.java
@@ -1,93 +1,159 @@
 package cucumber;
 
 
-
+import bank.Bank;
+import bank.BankSOAP;
+import bank.dtu.ws.fastmoney.exceptions.BankServiceException_Exception;
 import cucumber.api.java.en.Given;
-import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
-import static org.junit.Assert.*;
+import cucumber.api.java.en.When;
+import models.User;
 
-class IsItFriday {
-    static String isItFriday(String today) {
-        return "Nope";
-    }
-}
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+
+
 
 public class Stepdefs {
-    private String today;
-    private String actualAnswer;
-    private String cpr;
-    private String firstName;
-    private String lastName;
 
-
-
-    @Given("^today is Sunday$")
-    public void today_is_Sunday() {
-        today = "Sunday";
-    }
-
-    @When("^I ask whether it's Friday yet$")
-    public void i_ask_whether_is_s_Friday_yet() {
-        actualAnswer = IsItFriday.isItFriday(today);
-    }
-
-    @Then("^I should be told \"([^\"]*)\"$")
-    public void i_should_be_told(String expectedAnswer) {
-        System.out.println("Is it friday - Working");
-        assertEquals(expectedAnswer, actualAnswer);
-    }
-
-
+    private BigDecimal balance;
+    private Bank bank = new BankSOAP();
+    private User user = new User();
 
 
     @Given("that the user has cprNumber {string}")
     public void that_the_user_has_cprNumber(String cprInput) {
-        cpr = cprInput;
+        user.setCprNumber(cprInput);
     }
 
     @Given("that the user has firstName {string}")
     public void that_the_user_has_firstName(String firstNameInput) {
-        firstName = firstNameInput;
+        user.setFirstName(firstNameInput);
 
     }
 
     @Given("that the user has lastName {string}")
     public void that_the_user_has_lastName(String lastNameInput) {
-        lastName = lastNameInput;
+        user.setLastName(lastNameInput);
     }
 
     @Then("user cpr should be {string}")
     public void user_cpr_should_be(String expectedAnswer) {
         System.out.println("User cpr check - Working");
-        assertEquals(expectedAnswer, cpr);
+        assertEquals(expectedAnswer, user.getCprNumber());
     }
 
     @Given("that the balance is {string}")
-    public void that_the_balance_is(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    public void that_the_balance_is(String balanceInput) {
+        balance = new BigDecimal(Integer.parseInt(balanceInput));
     }
 
     @When("the user creates account")
     public void the_user_creates_account() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        try {
+            bank.createAccountWithBalance(user,balance);
+        } catch (BankServiceException_Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Then("the account is not {string}")
-    public void the_account_is_not(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    public void the_account_is_not(String nullInput) {
+        try {
+            assertNotSame(nullInput, bank.getAccountByCprNumber(user.getCprNumber()));
+        } catch (BankServiceException_Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Then("the account id is not {string}")
-    public void the_account_id_is_not(String string) {
+    public void the_account_id_is_not(String emptyString) {
+        try {
+            assertNotSame(emptyString, bank.getAccountByCprNumber(user.getCprNumber()));
+        } catch (BankServiceException_Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Given("the account is created")
+    public void the_account_is_created() {
         // Write code here that turns the phrase above into concrete actions
         throw new cucumber.api.PendingException();
     }
 
+    @When("the user gets account")
+    public void the_user_gets_account() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
 
+    @Then("the account id is correct")
+    public void the_account_id_is_correct() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the account balance is {string}")
+    public void the_account_balance_is(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the account user is correct")
+    public void the_account_user_is_correct() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the account transactions is empty")
+    public void the_account_transactions_is_empty() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("the user gets account by cpr number")
+    public void the_user_gets_account_by_cpr_number() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the account is not Null")
+    public void the_account_is_not_Null() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("the user gets accounts")
+    public void the_user_gets_accounts() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("account infos is not {string}")
+    public void account_infos_is_not(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("account infos is not empty")
+    public void account_infos_is_not_empty() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("account info is not Null")
+    public void account_info_is_not_Null() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("account info user is correct")
+    public void account_info_user_is_correct() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
 
 
 }
--- dtupay-bank/src/test/java/resources/cucumber/bank.feature
+++ dtupay-bank/src/test/java/resources/cucumber/bank.feature
@@ -1,11 +1,6 @@
 Feature: Bank cucumber tests.
   This run all test in regards to the bank.
 
-  Scenario: Sunday isn't Friday
-    Given today is Sunday
-    When I ask whether it's Friday yet
-    Then I should be told "Nope"
-
 
   Scenario: User creates account
     Given that the user has cprNumber "995566-2233"
@@ -15,3 +10,41 @@ Feature: Bank cucumber tests.
     When the user creates account
     Then the account is not "Null"
     And the account id is not ""
+
+  Scenario: User gets account
+    Given that the user has cprNumber "995566-2233"
+    And that the user has firstName "firstName"
+    And that the user has lastName "lastName"
+    And that the balance is "1000"
+    And the account is created
+    When the user gets account
+    Then the account is not "Null"
+    And the account id is correct
+    And the account balance is "1000"
+    And the account user is correct
+    And the account transactions is empty
+
+  Scenario: User gets account by cpr number
+    Given that the user has cprNumber "995566-2233"
+    And that the user has firstName "firstName"
+    And that the user has lastName "lastName"
+    And that the balance is "1000"
+    And the account is created
+    When the user gets account by cpr number
+    Then the account is not Null
+    And the account id is correct
+    And the account balance is "1000"
+    And the account user is correct
+    And the account transactions is empty
+
+
+  Scenario: User gets accounts
+    Given that the user has cprNumber "995566-2233"
+    And that the user has firstName "firstName"
+    And that the user has lastName "lastName"
+    And that the balance is "1000"
+    When the user gets accounts
+    Then account infos is not "Null"
+    And account infos is not empty
+    And account info is not Null
+    And account info user is correct
