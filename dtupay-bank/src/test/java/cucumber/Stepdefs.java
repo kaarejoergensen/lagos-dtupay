a4bfb4e21d640c634dcde00c1c9f69e9a51e0bd0
Asge Darr <asgedarr@Asges-MBP.zeroconf.dk>
2019-01-19 14:59:26 +0100

    agurk minor test added

--- dtupay-bank/src/test/java/cucumber/Stepdefs.java
+++ dtupay-bank/src/test/java/cucumber/Stepdefs.java
@@ -16,6 +16,11 @@ class IsItFriday {
 public class Stepdefs {
     private String today;
     private String actualAnswer;
+    private String cpr;
+    private String firstName;
+    private String lastName;
+
+
 
     @Given("^today is Sunday$")
     public void today_is_Sunday() {
@@ -29,6 +34,60 @@ public class Stepdefs {
 
     @Then("^I should be told \"([^\"]*)\"$")
     public void i_should_be_told(String expectedAnswer) {
+        System.out.println("Is it friday - Working");
         assertEquals(expectedAnswer, actualAnswer);
     }
+
+
+
+
+    @Given("that the user has cprNumber {string}")
+    public void that_the_user_has_cprNumber(String cprInput) {
+        cpr = cprInput;
+    }
+
+    @Given("that the user has firstName {string}")
+    public void that_the_user_has_firstName(String firstNameInput) {
+        firstName = firstNameInput;
+
+    }
+
+    @Given("that the user has lastName {string}")
+    public void that_the_user_has_lastName(String lastNameInput) {
+        lastName = lastNameInput;
+    }
+
+    @Then("user cpr should be {string}")
+    public void user_cpr_should_be(String expectedAnswer) {
+        System.out.println("User cpr check - Working");
+        assertEquals(expectedAnswer, cpr);
+    }
+
+    @Given("that the balance is {string}")
+    public void that_the_balance_is(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("the user creates account")
+    public void the_user_creates_account() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the account is not {string}")
+    public void the_account_is_not(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the account id is not {string}")
+    public void the_account_id_is_not(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+
+
+
 }
--- dtupay-bank/src/test/java/resources/cucumber/bank.feature
+++ dtupay-bank/src/test/java/resources/cucumber/bank.feature
@@ -1,7 +1,17 @@
-Feature: Is it Friday yet?
-  Everybody wants to know when it's Friday
+Feature: Bank cucumber tests.
+  This run all test in regards to the bank.
 
   Scenario: Sunday isn't Friday
     Given today is Sunday
     When I ask whether it's Friday yet
     Then I should be told "Nope"
+
+
+  Scenario: User creates account
+    Given that the user has cprNumber "995566-2233"
+    And that the user has firstName "firstName"
+    And that the user has lastName "lastName"
+    And that the balance is "1000"
+    When the user creates account
+    Then the account is not "Null"
+    And the account id is not ""
