7ad578d722951cc7fff9309fc3d62c349e2d8ad0
Kåre Jørgensen <kaare1994@gmail.com>
2019-01-20 17:44:13 +0100

    Added RPC tests

---
+++ dtupay-bank/src/test/java/BankRPCTest.java
@@ -0,0 +1,212 @@
+import base.RPCServer;
+import clients.BankClient;
+import com.palantir.docker.compose.DockerComposeRule;
+import com.palantir.docker.compose.connection.waiting.HealthChecks;
+import exceptions.ClientException;
+import models.Account;
+import models.AccountInfo;
+import models.Transaction;
+import models.User;
+import org.junit.After;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+
+public class BankRPCTest {
+    private BankClient bank;
+    private List<String> createdAccounts;
+
+    public BankRPCTest() throws TimeoutException, InterruptedException {
+        createdAccounts = new ArrayList<>();
+        RPCServer rpcServer = new Server();
+        System.out.println("Starting server");
+        new Thread(() -> {
+            try {
+                rpcServer.run("rabbitmq", Server.RPC_QUEUE_NAME + "-test");
+            } catch (IOException | TimeoutException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        System.out.println("Started in new thread");
+        while (true) {
+            try {
+                bank = new BankClient("rabbitmq", Server.RPC_QUEUE_NAME + "-test");
+                System.out.println("Created BankClient");
+                break;
+            } catch (IOException e) {
+                System.out.println("Client: Connection refused, waiting 5 seconds");
+                Thread.sleep(5000);
+            }
+        }
+    }
+
+    @Test
+    public void createAccount() throws ClientException {
+        System.out.println("Entering createAccount");
+        User user = new User("995566-2233","FirstName","LastName");
+        BigDecimal balance = new BigDecimal(1000);
+
+        this.ensureUserDoesntExist(user);
+        String accountId = this.bank.createAccountWithBalance(user, balance);
+        assertThat(accountId, is(notNullValue()));
+        assertThat(accountId, is(not("")));
+        this.createdAccounts.add(accountId);
+    }
+
+    @Test
+    public void getAccount() throws ClientException {
+        User user = new User("995566-2233","FirstName","LastName");
+        BigDecimal balance = new BigDecimal(1000);
+
+        this.ensureUserDoesntExist(user);
+        String accountId = this.bank.createAccountWithBalance(user, balance);
+        assertThat(accountId, is(notNullValue()));
+        assertThat(accountId, is(not("")));
+        this.createdAccounts.add(accountId);
+
+        Account account = this.bank.getAccount(accountId);
+        assertThat(account, is(notNullValue()));
+        assertThat(account.getId(), is(accountId));
+        assertThat(account.getBalance(), is(balance));
+        assertThat(account.getUser(), is(user));
+        assertThat(account.getTransactions(), is(Collections.emptyList()));
+    }
+
+    @Test
+    public void getAccountByCPR() throws ClientException {
+        User user = new User("995566-2233","FirstName","LastName");
+        BigDecimal balance = new BigDecimal(1000);
+
+        this.ensureUserDoesntExist(user);
+        String accountId = this.bank.createAccountWithBalance(user, balance);
+        assertThat(accountId, is(notNullValue()));
+        assertThat(accountId, is(not("")));
+        this.createdAccounts.add(accountId);
+
+        Account account = this.bank.getAccountByCprNumber(user.getCprNumber());
+        assertThat(account, is(notNullValue()));
+        assertThat(account.getId(), is(accountId));
+        assertThat(account.getBalance(), is(balance));
+        assertThat(account.getUser(), is(user));
+        assertThat(account.getTransactions(), is(Collections.emptyList()));
+    }
+
+    @Test
+    public void getAccounts() throws ClientException {
+        User user = new User("995566-2233","FirstName","LastName");
+        BigDecimal balance = new BigDecimal(1000);
+
+        this.ensureUserDoesntExist(user);
+        String accountId = this.bank.createAccountWithBalance(user, balance);
+        assertThat(accountId, is(notNullValue()));
+        assertThat(accountId, is(not("")));
+        this.createdAccounts.add(accountId);
+
+        List<AccountInfo> accountInfos = this.bank.getAccounts();
+        assertThat(accountInfos, is(notNullValue()));
+        assertThat(accountInfos.isEmpty(), is(false));
+
+        AccountInfo accountInfo = accountInfos.stream()
+                .filter(ai -> ai.getAccountId().equals(accountId))
+                .findFirst().orElse(null);
+        assertThat(accountInfo, is(notNullValue()));
+        assertThat(accountInfo.getUser(), is(user));
+    }
+
+    @Test(expected = ClientException.class)
+    public void retireAccount() throws ClientException {
+        User user = new User("995566-2233","FirstName","LastName");
+        BigDecimal balance = new BigDecimal(1000);
+
+        this.ensureUserDoesntExist(user);
+        String accountId = this.bank.createAccountWithBalance(user, balance);
+        assertThat(accountId, is(notNullValue()));
+        assertThat(accountId, is(not("")));
+
+        this.bank.retireAccount(accountId);
+        this.bank.getAccount(accountId);
+    }
+
+    @Test
+    public void transferMoneyFromTo() throws ClientException {
+        User user1 = new User("995566-2233","FirstName","LastName");
+        User user2 = new User("662288-5522","FirstName","LastName");
+        BigDecimal balance = new BigDecimal(1000);
+
+        this.ensureUserDoesntExist(user1);
+        this.ensureUserDoesntExist(user2);
+        String accountId1 = this.bank.createAccountWithBalance(user1, balance);
+        String accountId2 = this.bank.createAccountWithBalance(user2, balance);
+
+        assertThat(accountId1, is(notNullValue()));
+        assertThat(accountId1, is(not("")));
+        this.createdAccounts.add(accountId1);
+        assertThat(accountId2, is(notNullValue()));
+        assertThat(accountId2, is(not("")));
+        this.createdAccounts.add(accountId2);
+
+        Account account1 = this.bank.getAccount(accountId1);
+        Account account2 = this.bank.getAccount(accountId2);
+
+        assertThat(account1, is(notNullValue()));
+        assertThat(account1.getBalance(), is(balance));
+        assertThat(account2, is(notNullValue()));
+        assertThat(account2.getBalance(), is(balance));
+
+        this.bank.transferMoneyFromTo(accountId1, accountId2, balance, "Test transfer");
+        account1 = this.bank.getAccount(accountId1);
+        account2 = this.bank.getAccount(accountId2);
+
+        assertThat(account1, is(notNullValue()));
+        assertThat(account1.getBalance(), is(new BigDecimal(0)));
+        assertThat(account2, is(notNullValue()));
+        assertThat(account2.getBalance(), is(new BigDecimal(2000)));
+
+        assertThat(account1.getTransactions(), is(notNullValue()));
+        assertThat(account1.getTransactions().size(), is(1));
+        Transaction transaction1 = account1.getTransactions().get(0);
+        assertThat(transaction1.getAmount(), is(balance));
+        assertThat(transaction1.getBalance(), is(new BigDecimal(0)));
+        assertThat(transaction1.getCreditor(), is(accountId2));
+        assertThat(transaction1.getDebtor(), is("this account"));
+        assertThat(transaction1.getDescription(), is("Test transfer"));
+
+        assertThat(account2.getTransactions(), is(notNullValue()));
+        assertThat(account2.getTransactions().size(), is(1));
+        Transaction transaction2 = account2.getTransactions().get(0);
+        assertThat(transaction2.getAmount(), is(balance));
+        assertThat(transaction2.getBalance(), is(new BigDecimal(2000)));
+        assertThat(transaction2.getCreditor(), is("this account"));
+        assertThat(transaction2.getDebtor(), is(accountId1));
+        assertThat(transaction2.getDescription(), is("Test transfer"));
+    }
+
+    private void ensureUserDoesntExist(User user) {
+        try {
+            Account account = this.bank.getAccountByCprNumber(user.getCprNumber());
+            this.bank.retireAccount(account.getId());
+        } catch (ClientException e) {
+        }
+    }
+
+    @After
+    public void tearDown() {
+        for (String id : this.createdAccounts) {
+            try {
+                this.bank.retireAccount(id);
+            } catch (ClientException e) {
+            }
+        }
+        this.createdAccounts.clear();
+    }
+}
---
+++ dtupay-token/src/test/java/TokenRPCTest.java
@@ -0,0 +1,120 @@
+import base.RPCServer;
+import clients.TokenClient;
+import com.palantir.docker.compose.DockerComposeRule;
+import com.palantir.docker.compose.connection.waiting.HealthChecks;
+import exceptions.ClientException;
+import org.junit.After;
+import org.junit.ClassRule;
+import org.junit.Test;
+import persistence.MemoryDataStore;
+import tokens.TokenProvider;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeoutException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class TokenRPCTest {
+    private List<String> tokensToBeUsed = new ArrayList<>();
+
+    private String userName = "core";
+    private String userId = "1234";
+
+    private TokenClient tokenClient;
+
+    public TokenRPCTest() throws TimeoutException, InterruptedException {
+        TokenProvider tokenProvider = new TokenProvider(new MemoryDataStore());
+        RPCServer rpcServer = new Server(tokenProvider);
+        System.out.println("Starting server");
+        new Thread(() -> {
+            try {
+                rpcServer.run("rabbitmq", Server.RPC_QUEUE_NAME + "-test");
+            } catch (IOException | TimeoutException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        System.out.println("Started in new thread");
+        while (true) {
+            try {
+                tokenClient = new TokenClient("rabbitmq", Server.RPC_QUEUE_NAME + "-test");
+                System.out.println("Created TokenClient");
+                break;
+            } catch (IOException e) {
+                System.out.println("Client: Connection refused, waiting 5 seconds");
+                Thread.sleep(5000);
+            }
+        }
+    }
+
+    @Test
+    public void issueTokens() throws ClientException {
+        Set<String> tokens = tokenClient.getTokens(this.userName, this.userId, 5);
+        assertThat(tokens, is(notNullValue()));
+        assertThat(tokens.size(), is(5));
+        for (String token : tokens) {
+            assertThat(tokenClient.useToken(token), is(true));
+        }
+    }
+
+    @Test
+    public void invalidToken() throws ClientException {
+        assertThat(tokenClient.useToken("1234"), is(false));
+    }
+
+    @Test
+    public void tamperedToken() throws ClientException {
+        Set<String> tokens = tokenClient.getTokens(this.userName, this.userId, 1);
+        assertThat(tokens, is(notNullValue()));
+        assertThat(tokens.size(), is(1));
+        assertThat(tokenClient.useToken(tokens.iterator().next() + "1234"), is(false));
+        this.tokensToBeUsed.addAll(tokens);
+    }
+
+    @Test(expected = ClientException.class)
+    public void maxFiveTokens() throws ClientException {
+        tokenClient.getTokens(this.userName, this.userId, 6);
+    }
+
+    @Test(expected = ClientException.class)
+    public void atLeastOneToken() throws ClientException {
+        tokenClient.getTokens(this.userName, this.userId, 0);
+    }
+
+    @Test
+    public void useToken() throws ClientException {
+        Set<String> tokens = tokenClient.getTokens(this.userName, this.userId, 1);
+        assertThat(tokens, is(notNullValue()));
+        assertThat(tokens.size(), is(1));
+        assertThat(tokenClient.useToken(tokens.iterator().next()), is(true));
+        assertThat(tokenClient.useToken(tokens.iterator().next()), is(false));
+    }
+
+    @Test
+    public void getTokenIfOnlyOneUnused() throws ClientException {
+        Set<String> tokens = tokenClient.getTokens(this.userName, this.userId, 1);
+        tokens.addAll(tokenClient.getTokens(this.userName, this.userId, 1));
+        assertThat(tokens, is(notNullValue()));
+        assertThat(tokens.size(), is(2));
+        tokenClient.useToken(tokens.iterator().next());
+        tokens.addAll(tokenClient.getTokens(this.userName, this.userId, 1));
+        tokensToBeUsed.addAll(tokens);
+    }
+
+    @Test(expected = ClientException.class)
+    public void maxOneUnusedToken() throws ClientException {
+        tokensToBeUsed.addAll(tokenClient.getTokens(this.userName, this.userId, 2));
+        tokenClient.getTokens(this.userName, this.userId, 1);
+    }
+
+    @After
+    public void deleteTokens() throws ClientException {
+        for (String token : this.tokensToBeUsed)
+            this.tokenClient.useToken(token);
+        this.tokensToBeUsed.clear();
+    }
+}
