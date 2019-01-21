2b7ef921424176756d088c48461b6e9aa9eff7cf
s172189 <s172189@student.dtu.dk>
2019-01-21 12:35:54 +0100

    Added more tests for tokens

--- dtupay-token/src/test/java/tokencucomber/IssueToken.java
+++ dtupay-token/src/test/java/tokencucomber/IssueToken.java
@@ -1,14 +1,17 @@
 package tokencucomber;
 
 import cucumber.api.PendingException;
+import cucumber.api.Transform;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import persistence.Datastore;
 import persistence.MemoryDataStore;
+import persistence.MongoDataStore;
 import tokens.TokenProvider;
 
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -43,11 +46,57 @@ public class IssueToken {
         tokens = tokenProvider.getTokens(userName,userId,numberOfTokens);
     }
 
+    @Then("^the tokens is not null")
+    public void the_tokens_is_not_null(){
+        assertThat(tokens, is(notNullValue()) );
+    }
 
-    @Then("^the tokens provided should be \"([^\"]*)\"$")
-    public void the_tokens_provided_should_not_be(int expectedValue){
+
+    @Then("^the token count provided should be \"([^\"]*)\"$")
+    public void the_token_count_provided_should_be(int expectedValue){
         assertEquals(tokens.size(), expectedValue);
     }
+
+    @Then("^the user is able to use the tokens")
+    public void the_user_is_able_to_use_the_tokens(){
+        for (String token : tokens) {
+            assertThat(tokenProvider.useToken(token), is(true));
+        }
+    }
+
+    @Then("^not use tokens beyond the provided ones")
+    public void not_use_tokens_beyond_the_provided_ones(){
+        assertThat(tokenProvider.useToken("1234"), is(false));
+    }
+
+    /*
+    SCENARIO 2
+     */
+
+    @Given("^username \"([^\"]*)\", userId \"([^\"]*)\" and \"([^\"]*)\" number of tokens$")
+    public void tampring_given_username_id_nrtokens(String username, String userid, int nrOfTokens ) {
+        this.userName = username;
+        this.userId = userid;
+        this.numberOfTokens = nrOfTokens;
+    }
+
+    @When("^retreving a set of tokens$")
+    public void tampring_retriving_tokens() {
+        datastore = new MemoryDataStore();
+        tokenProvider = new TokenProvider(datastore);
+        tokens = this.tokenProvider.getTokens(this.userName, this.userId, this.numberOfTokens);
+    }
+
+    @Then("^the token count is indeed \"([^\"]*)\"$")
+    public void tampring_check_number_and_notnull(int expectedNr){
+        assertEquals(tokens.size(), expectedNr);
+    }
+
+    @Then("^the user can't tamper the token  - use a non existing token by injecting userid$")
+    public void cant_tamper_token(){
+        assertThat(tokenProvider.useToken(tokens.iterator().next() + this.userId), is(false));
+    }
+
 
 
 
--- dtupay-token/src/test/resources/tokencucomber/tokenCuc.feature
+++ dtupay-token/src/test/resources/tokencucomber/tokenCuc.feature
@@ -1,9 +1,18 @@
 Feature: Token and Storage test
   Check if the different features in Token API run as intended
 
-  Scenario: Issue A token
+  Scenario: Standard token issuing and using
     Given the user id, user name and the number of tokens
     When issuing tokens
-    Then the tokens provided should be "5"
+    Then the tokens is not null
+    And the token count provided should be "5"
+    And the user is able to use the tokens
+    And not use tokens beyond the provided ones
+
+   Scenario: Tampering Tokens
+    Given username "bro", userId "8273" and "1" number of tokens
+    When retreving a set of tokens
+    Then the token count is indeed "1"
+    And the user can't tamper the token  - use a non existing token by injecting userid
 
 
